ef52d7694a150f7e0f26866ea9f93f040c95799e
arth1312 <arthkoradiya@gmail.com>
2024-12-19 18:06:45 +0530

    custom_pattern 19/12/2024

---
+++ alphabetic_pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int i;
+   for(i = 1; i <= 6; i++)
+   {
+    if(i == 1 || i == 3)
+    {
+        cout << "* * * * * *";
+    }
+    else
+    {
+        cout << "*         *";
+    }
+    cout << endl;
+   }
+    return 0;
+}
