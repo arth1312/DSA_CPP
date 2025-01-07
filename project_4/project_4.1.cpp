6e486436f3c2b7b31d24e26bfcd608b3403b6cc4
arth1312 <arthkoradiya@gmail.com>
2025-01-07 14:46:37 +0530

    project_4.1.cpp 06/01/2025

--- project_4/project_4.1.cpp
+++ project_4/project_4.1.cpp
@@ -1,55 +1,64 @@
 #include<iostream>
 using namespace std;
+
 void calculator()
 {
-	cout << "Press 1 for +" << endl;
-	cout << "Press 2 for -" << endl;
-	cout << "Press 3 for *" << endl;
-	cout << "Press 4 for /" << endl;
-	cout << "Press 5 for %" << endl;
-	cout << "Press 0 for the exit" << endl;
-	int choice;
-	cout << "Enter your choice: ";
-	cin >> choice;
-	int first, second;
-	cout << "Enter the first number: ";
-	cin >> first;
-	cout << "Enter the second number: ";
-	cin >> second;
-	int sum = 0;
-	do
-	{
-		switch(choice)
-		{
-			case 1:	
-				cout << "Addition of "<< first <<"and "<< second <<" is " << first + second << endl;
-			break;
-			
-			case 2:
-				cout << "Subtraction of "<< first <<"and "<< second <<" is " << first - second << endl;
-			break;
-			
-			case 3:
-				cout << "Multiplication of "<< first <<"and "<< second <<" is " << first * second << endl;
-			break;
-			
-			case 4:
-				cout << "Division of "<< first <<"and "<< second <<" is " << first / second << endl;
-			break;
-			
-			case 5:
-				cout << "Moduls of "<< first <<"and "<< second <<" is " << first % second << endl;
-			break;
-			
-			case 0:
-			break;
-			
-			default:
-				cout << "Enter valid choice" <<endl;
-			break;
-		}
-	} while(choice != 0);
+    int choice;
+    do
+    {
+        cout << "Press 1 for +" << endl;
+        cout << "Press 2 for -" << endl;
+        cout << "Press 3 for *" << endl;
+        cout << "Press 4 for /" << endl;
+        cout << "Press 5 for %" << endl;
+        cout << "Press 0 for the exit" << endl;
+
+        cout << endl << "=======================================" << endl << endl;
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 0) {
+            break;
+        }
+
+        int first, second;
+        cout << "Enter the first number: ";
+        cin >> first;
+        cout << "Enter the second number: ";
+        cin >> second;
+
+        switch(choice)
+        {
+            case 1:
+                cout << "Addition of " << first << " and " << second << " is " << first + second << endl << endl;
+                break;
+            
+            case 2:
+                cout << "Subtraction of " << first << " and " << second << " is " << first - second << endl << endl;
+                break;
+            
+            case 3:
+                cout << "Multiplication of " << first << " and " << second << " is " << first * second << endl << endl;
+                break;
+            
+            case 4:
+                if (second != 0)
+                    cout << "Division of " << first << " and " << second << " is " << first / second << endl << endl;
+                break;
+            
+            case 5:
+                if (second != 0)
+                    cout << "Modulus of " << first << " and " << second << " is " << first % second << endl << endl;
+                break;
+            
+            default:
+                cout << "Enter a valid choice" << endl;
+                break;
+        }
+    } while(choice != 0);
 }
+
 int main()
 {
     calculator();
