1c1888409a545b0a478e43c4e9a6418265240e6c
arth1312 <arthkoradiya@gmail.com>
2025-01-09 18:22:33 +0530

    OOP 09/01/2025

---
+++ 18_OOP/oop.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Student
+{
+    public:
+        int grid;
+        char name[50];
+        int age;
+        char course[50];
+        float persentage;
+};
+
+int main()
+{
+    Student student1;
+
+    student1.grid = 258;
+    strcpy(student1.name, "Harsh");
+    student1.age = 19;
+    strcpy(student1.course, "DSA");
+    student1.persentage = 90.88;
+
+    cout << "Grid: " << student1.grid << endl;
+    cout << "Name: " << student1.name << endl;
+    cout << "Age: " << student1.age << endl;
+    cout << "Course: " << student1.course << endl;
+    cout << "Persentage: " << student1.persentage << endl;
+
+    return 0;
+} 
