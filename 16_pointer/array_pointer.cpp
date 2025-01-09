bdef994286f833f5693dd0d10e73df2f694713b0
arth1312 <arthkoradiya@gmail.com>
2025-01-09 18:07:15 +0530

    pointer 07/01/2025

---
+++ 16_pointer/array_pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a[5] = {55, 33, 14, 85, 99};
+   int *p[5];
+   int i;
+   for(i = 0; i <= 4; i++)
+   {
+    *p[i] = &a[i];
+   }
+
+   for(i = 0; i <= 4; i++)
+   {
+    cout << "Address is " << p[i];
+   }
+    return 0;
+}
---
+++ 16_pointer/chain_pointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a = 5;
+   int *p;
+   int **p1;
+   *p = &a;
+   **p1 = &p;
+   cout << a;
+   cout << *p;
+   cout << **p1;
+    return 0;
+}
