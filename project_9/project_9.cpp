6242de77680065e9672693d9d8ca5abba69f8130
arth1312 <arthkoradiya@gmail.com>
2025-02-15 18:31:00 +0530

    project_9.cpp 15/02/2025

--- project_9/project_9.cpp
+++ project_9/project_9.cpp
@@ -1,134 +1,120 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class StackBase
-{
-	public:
-		virtual void push(int value) = 0;
-		virtual void pop() = 0;
-		virtual void top() = 0;
-		virtual bool isEmpty() = 0;
-		virtual bool isFull() = 0;
-		virtual ~StackBase() {}
+class Stack {
+protected:
+    int* arr;     
+    int topIndex;  
+    int capacity;  
+
+public:
+    Stack(int size) {
+        capacity = size;
+        arr = new int[capacity];
+        topIndex = -1;
+    }
+
+    void push(int value) {
+        if (isFull()) {
+            cout << "Stack is full! Cannot push " << value << endl;
+            return;
+        }
+        arr[++topIndex] = value;
+        cout << value << " pushed onto the stack." << endl;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty! Cannot pop." << endl;
+            return -1;
+        }
+        return arr[topIndex--];
+    }
+
+    int top() {
+        if (isEmpty()) {
+            cout << "Stack is empty! No top element." << endl;
+            return -1;
+        }
+        return arr[topIndex];
+    }
+
+    bool isEmpty() {
+        return topIndex == -1;
+    }
+
+    bool isFull() {
+        return topIndex == capacity - 1;
+    }
+
+    ~Stack() {
+        delete[] arr;
+    }
 };
 
-class Stack : public StackBase
-{
-	private:
-		int* arr;
-		int maxSize;
-		int topIndex;
-		
-	public:
-		Stack(int size)
-		{
-			maxSize = size;
-			arr = new int [maxSize];
-			topIndex = -1;
-		}
-		
-		~Stack()
-		{
-			delete[] arr;
-		}
-		
-		void push(int value)
-		{
-			if (isFull())
-			{
-				cout << "Stack is full! Cannot push" << value << endl;
-			}
-			else
-			{
-				arr[topIndex++] = value;
-				cout << value << "pushed on to Stack" << endl;
-			}
-		}
-		
-		void pop()
-		{
-			if(isEmpty())
-			{
-				cout << "Stack is empty! Nothing to pop" << endl;
-			}
-			else
-			{
-				cout << arr[topIndex--] << "Stack is poped" << endl;
-			}
-		}
-		
-		int top()
-		{
-			if(isEmpty())
-			{
-				cout << "Stack is empty!" << endl;
-				return -1;
-			}
-			return arr[topIndex];
-		}
-		
-		bool isEmpty()
-		{
-			return topIndex == -1;
-		}
-		
-		bool isFull() override
-		{
-			return topIndex == maxSize - 1;
-		}
+class CustomStack : public Stack {
+public:
+    CustomStack(int size) : Stack(size) {}
+
+    void push() {
+        if (isFull()) {
+            cout << "Stack is full! Cannot push." << endl;
+            return;
+        }
+        int value;
+        cout << "Enter a value to push: ";
+        cin >> value;
+        Stack::push(value);
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty! Nothing to pop." << endl;
+        } else {
+            cout << "Popped value: " << Stack::pop() << endl;
+        }
+    }
+
+    void displayTop() {
+        int topValue = Stack::top();
+        if (topValue != -1) {
+            cout << "Top element: " << topValue << endl;
+        }
+    }
+
+    void checkEmpty() {
+        cout << (isEmpty() ? "Stack is empty." : "Stack is not empty.") << endl;
+    }
+
+    void checkFull() {
+        cout << (isFull() ? "Stack is full." : "Stack is not full.") << endl;
+    }
 };
 
-void menu(Stack& stack)
-{
-	int choice, value;
-	while(true)
-	{
-		cout << endl << "Stack Operation" << endl;
-		cout << "Press 1 stack is push" << endl;
-		cout << "Press 2 stack is pop" << endl;
-		cout << "Press 3 stack is display top" << endl;
-		cout << "Press 4 stack is check empty" << endl;
-		cout << "Press 5 stack is check full" << endl;
-		cout << "Press 0 stack is exit" << endl;
-		
-		cout << "Enter your choice: ";
-		cin >> choice;
-		
-		switch(choice)
-		{
-			case 1:
-				cout << "Enter the value of push: ";
-				cin >> value;
-				Stack.push(value);
-				break;
-			case 2:
-				Stack.pop();
-				break;
-			case 3:
-				Stack.top();
-				break;
-			case 4:
-				Stack.isEmpty();
-				break;
-			case 5:
-				Stack.isFull();
-				break;
-			case 0:
-				cout << "Stack is exit" << endl;
-				break;
-			default:
-				cout << "Your invalid choice" << endl;
-				break;
-		}
-	}
-}
+int main() {
+    int size;
+    cout << "Enter stack size: ";
+    cin >> size;
 
-int main()
-{
-	int size;
-	cout << "Enter Stack size: ";
-	cin >> size;
-	Stack stack(size);
-	menu(stack);
-	return 0;
+    CustomStack myStack(size);
+    int choice;
+
+    while (true) {
+        cout << "\nStack Operations Menu:\n";
+        cout << "1. Push\n2. Pop\n3. Display Top\n4. Check if Empty\n5. Check if Full\n6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: myStack.push(); break;
+            case 2: myStack.pop(); break;
+            case 3: myStack.displayTop(); break;
+            case 4: myStack.checkEmpty(); break;
+            case 5: myStack.checkFull(); break;
+            case 6: cout << "Exiting program...\n"; return 0;
+            default: cout << "Invalid choice! Try again.\n";
+        }
+    }
+
+    return 0;
 }
